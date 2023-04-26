74f1cdb8d93210ae2d356fbcfb2cb8e55ad5a8f8
Nazarian <nazariansani@gmail.com>
2023-04-26 21:41:20 +0330

    using AHT10 Sensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,57 +2,37 @@
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
 #include <Servo.h>
-#include <DHT.h>
+#include <AHTxx.h>
+#include <Wire.h>
 
 void setTime();
 void setTempratureAndHumidity();
 void goToSetStatus();
 void readSensors();
-int average(int *array, int len);
+int average (int * array, int len);
+void Siren(bool Enabled);
 
 #define lampPin 3
 #define fanPin 6
 #define speakerPin 7
-#define DHT11_PIN 4
-#define DHTTYPE DHT11
 
-DHT dht(DHT11_PIN, DHTTYPE);
+#define ok A0
+#define UP A1
+#define DOWN A2
+#define servoPin A3
 
-const byte probe = 5; // reading probes for average temp and humidity
-// globals
-int index = 0;
-int baselineTemp;
-int baselineHum;
-int tempArr[probe];
-int humArr[probe];
-
-void readSensors()
-{
-  // DHT reading
-  tempArr[index] = dht.readTemperature();
-  humArr[index] = dht.readHumidity();
-
-  // get average
-  baselineTemp = average(tempArr, probe);
-  baselineHum = average(humArr, probe);
-}
-// assuming array is int
-int average(int *array, int len)
-{
-  long sum = 0L; // sum will be larger than an item, long for safety.
-  for (int i = 0; i < len; i++)
-    sum += array[i];
-  return ((int)sum) / len; // average will be fractional, so float may be appropriate.
-}
+// for AHT10 Sensor
+uint8_t readStatus = 0;
+AHTxx AHT10Sensor(AHTXX_ADDRESS_X38, AHT1x_SENSOR);
 
 // for EEPROM
 // Addreses for EEPROM
-int secondAddr = 0, minuteAddr = 1, hourAddr = 2, dayAddr = 3;
-int ServoPositionAddr = 4, T_thresholdAddr = 5, H_thresholdAddr = 6;
+byte secondAddr = 0, minuteAddr = 1, hourAddr = 2, dayAddr = 3;
+byte ServoPositionAddr = 4, T_thresholdAddr = 5, H_thresholdAddr = 6;
 
 // Servo //
 Servo servo;
-int servoPosition = 0;
+byte servoPosition = 0;
 
 // for LCD Display Pins //
 const byte rs = 8, en = 9, d4 = 10, d5 = 11, d6 = 12, d7 = 13;
@@ -76,11 +56,15 @@ byte HumidityIcon[8] = {
     B11111,
     B11111,
     B01110};
-
-// Buttons //
-const int ok = A1;
-const int UP = A2;
-const int DOWN = A3;
+byte degreIcon[8] = {
+    B11000,
+    B11000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000};
 
 // Speaker //
 float sinVal;
@@ -96,11 +80,14 @@ unsigned long timeTemp = 0;
 unsigned long timeDetail = 0;
 
 // Initial Value for Tempurature and Humidity //
-int T_threshold = 0;
-int H_threshold = 0;
+float T_threshold = 0;
+float H_threshold = 0;
+
+// Save Temp and Humidity
+float baselineTemp;
+float baselineHum;
 
 // for Status //
-// byte ack = 0;
 byte SET = 1;
 byte timeStatus = 0;
 byte AutoSet = 0;
@@ -125,9 +112,9 @@ void setup()
   digitalWrite(lampPin, LOW);
   analogWrite(fanPin, 0);
   digitalWrite(speakerPin, LOW);
-  digitalWrite(ok, HIGH);
-  digitalWrite(UP, HIGH);
-  digitalWrite(DOWN, HIGH);
+  digitalWrite(ok, HIGH);   // WHAT?
+  digitalWrite(UP, HIGH);   // WHAT?
+  digitalWrite(DOWN, HIGH); // WHAT?
 
   Sec = EEPROM.read(secondAddr);
   Min = EEPROM.read(minuteAddr);
@@ -138,10 +125,12 @@ void setup()
   servoPosition = EEPROM.read(ServoPositionAddr);
 
   lcd.begin(16, 2);
+
   lcd.createChar(0, TempIcon);
   lcd.createChar(1, HumidityIcon);
-  dht.begin();
-  servo.attach(A4);
+  lcd.createChar(2, degreIcon);
+
+  servo.attach(servoPin);
   Serial.begin(9600);
 
   while (servoPosition > 0)
@@ -155,286 +144,255 @@ void setup()
 
   lcd.clear();
   lcd.print("Incubator");
-  Serial.println("> Temperature and Humidity Controller For Incubator <");
+  Serial.println(F("> Temperature and Humidity Controller For Incubator <"));
   delay(1000);
   lcd.clear();
-  lcd.print("Calibrating");
+  lcd.print("Testing");
   lcd.setCursor(0, 1);
   lcd.print("sensor...");
-  Serial.println("Calibrating sensors...");
+  Serial.println(F("Testing sensor..."));
 
-  // calibration
-  for (index = 0; index < probe; index = index + 1)
+  while (AHT10Sensor.begin() != true)
   {
-    readSensors();
-    delay(700);
+    Serial.println(F("AHT10 not connected or fail to load calibration coefficient"));
+    //(F()) save string to flash & keeps dynamic memory free
+    lcd.clear();
+    lcd.print("AHT10 error");
+    delay(5000);
   }
-
   lcd.clear();
   lcd.print("Ready");
-  Serial.println("Ready");
-  index = 0;
+  Serial.println(F("Ready"));
   delay(1000);
   lcd.clear();
 }
 
 void loop()
 {
+  Siren(false);
+
   setTempratureAndHumidity();
   setTime();
   goToSetStatus();
 
   timeRunning = millis();
 
-  readSensors();
-  index = index + 1;
-  if (index > probe - 1)
-  {
-    index = 0;
-  }
-  //  int chk = DHTSensor.read11(DHT11_PIN);    // READ DATA
-  //    // Check DHTSensor //
-  //  switch (chk)
-  //  {
-  //      case DHTLIB_OK:
-  //      ack = 0;
-  //      break;
-  //      case DHTLIB_ERROR_CHECKSUM:
-  //      Serial.print("Sensor : Checksum error,\t");
-  //      ack = 1;
-  //      delay(200);
-  //      break;
-  //      case DHTLIB_ERROR_TIMEOUT:
-  //      Serial.print("Sensor : Time out error,\t");
-  //      ack = 1;
-  //      delay(200);
-  //      break;
-  //      default:
-  //      Serial.print("Sensor : Unknown error,\t");
-  //      ack = 1;
-  //      delay(200);
-  //      break;
-  //  }
+  baselineTemp = AHT10Sensor.readTemperature();
+  baselineHum = AHT10Sensor.readHumidity();
 
   // DISPLAY DATA in Serial Monitor
-  Serial.print("DHT11, \t");
-
+  Serial.print(F("AHT10, \t"));
   Serial.print(baselineTemp, 1);
-  Serial.print(",\t");
-
+  Serial.print(F(",\t"));
   Serial.print(baselineHum, 1);
-  Serial.print(",\t");
-  Serial.println(millis());
+  Serial.print(F(",\t"));
 
-  // if (ack == 0)
-  //{
-
-  lcd.home();
-  lcd.write((byte)0);
-  lcd.print(baselineTemp);
-  lcd.setCursor(5, 0);
-  lcd.print(" ");
-  lcd.print(T_threshold);
-
-  lcd.setCursor(8, 0);
-  lcd.print("  ");
-  lcd.write((byte)1);
-  lcd.print(baselineHum);
-  lcd.setCursor(13, 0);
-  lcd.print(" ");
-  lcd.print(H_threshold);
-  lcd.setCursor(1, 1);
-  lcd.print("d:");
-  lcd.print(Day);
-  lcd.setCursor(6, 1);
-  lcd.print("h:");
-  lcd.print(Hrs);
-  lcd.setCursor(11, 1);
-  lcd.print("m:");
-  lcd.print(Min);
-
-  if (baselineHum > H_threshold)
+  if (baselineTemp != AHTXX_ERROR)
   {
-    delaySensorValue = millis() - timeRunning;
-    delaySensorValue = 500 - delaySensorValue;
-    delay(delaySensorValue);
-
-    lcd.setCursor(13, 0);
-    lcd.print(">");
-    analogWrite(fanPin, 255);
-    // digitalWrite(fanPin, HIGH);
-  }
-  else if (baselineHum == H_threshold)
-  {
-    delaySensorValue = millis() - timeRunning;
-    delaySensorValue = 500 - delaySensorValue;
-    delay(delaySensorValue);
-
-    analogWrite(fanPin, 255);
-    lcd.setCursor(13, 0);
-    lcd.print("=");
-  }
-  else
-  {
-    delaySensorValue = millis() - timeRunning;
-    delaySensorValue = 500 - delaySensorValue;
-    delay(delaySensorValue);
-
-    analogWrite(fanPin, 0);
-    lcd.setCursor(13, 0);
-    lcd.print("<");
-    // digitalWrite(fanPin, LOW);
-  }
-
-  if (baselineTemp > T_threshold)
-  {
+    lcd.clear();
+    lcd.write((byte)0);
+    lcd.print(baselineTemp);
     lcd.setCursor(5, 0);
-    lcd.print(">");
-    delay(500);
-    while (baselineTemp - T_threshold > 5)
+    lcd.print(" ");
+    lcd.print(T_threshold);
+
+    lcd.setCursor(8, 0);
+    lcd.print("  ");
+    lcd.write((byte)1);
+    lcd.print(baselineHum);
+    lcd.setCursor(13, 0);
+    lcd.print(" ");
+    lcd.print(H_threshold);
+
+    lcd.setCursor(1, 1);
+    lcd.print("d:");
+    lcd.print(Day);
+    lcd.setCursor(6, 1);
+    lcd.print("h:");
+    lcd.print(Hrs);
+    lcd.setCursor(11, 1);
+    lcd.print("m:");
+    lcd.print(Min);
+
+    if (baselineHum > H_threshold)
     {
-      lcd.clear();
-      lcd.print("Temp is High!!!");
-      digitalWrite(lampPin, LOW);
+      delaySensorValue = millis() - timeRunning;
+      delaySensorValue = 500 - delaySensorValue;
+      delay(delaySensorValue);
+
+      lcd.setCursor(13, 0);
+      lcd.print(">");
       analogWrite(fanPin, 255);
-      for (int x = 0; x < 180; x++)
-      {
-        // convert degrees to radians then obtain sin value
-        sinVal = (sin(x * (3.1412 / 180)));
-        // generate a frequency from the sin value
-        toneVal = 2000 + (int(sinVal * 1000));
-        tone(7, toneVal);
-        delay(2);
-      }
-      goToSetStatus();
-      delay(640);
-      readSensors();
-      lcd.clear();
+      // digitalWrite(fanPin, HIGH);
     }
-    noTone(7);
-    //      if ( digitalRead(lampPin) == HIGH )
-    //      {
-    //        // baraye inke yek meghdar lampPin ro hanooz roshan negah dare ta bishtar garm beshe mohit.
-    //        delay(5000);
-    //      }
-    digitalWrite(lampPin, LOW);
-    delay(2000);
-  }
-  else if ((baselineTemp == T_threshold))
-  {
-    lcd.setCursor(5, 0);
-    lcd.print("=");
-    delay(500);
-    digitalWrite(lampPin, LOW);
+    else if (baselineHum == H_threshold)
+    {
+      delaySensorValue = millis() - timeRunning;
+      delaySensorValue = 500 - delaySensorValue;
+      delay(delaySensorValue);
+
+      analogWrite(fanPin, 255);
+      lcd.setCursor(13, 0);
+      lcd.print("=");
+    }
+    else
+    {
+      delaySensorValue = millis() - timeRunning;
+      delaySensorValue = 500 - delaySensorValue;
+      delay(delaySensorValue);
+
+      analogWrite(fanPin, 0);
+      lcd.setCursor(13, 0);
+      lcd.print("<");
+      // digitalWrite(fanPin, LOW);
+    }
+
+    if (baselineTemp > T_threshold)
+    {
+      while (baselineTemp - T_threshold > 5) // age 5 darage bishtar bood
+      {
+        lcd.clear();
+        lcd.print("Temp is High!!!");
+        digitalWrite(lampPin, LOW);
+        analogWrite(fanPin, 255);
+        Siren(true);
+        goToSetStatus();
+
+        delay(500);
+        lcd.setCursor(1, 3);
+        lcd.print(AHT10Sensor.readTemperature());
+        baselineTemp = AHT10Sensor.readTemperature();
+        lcd.print("C");
+      }
+      lcd.setCursor(5, 0);
+      lcd.print(">");
+      Siren(false);
+      digitalWrite(lampPin, LOW);
+      delay(2500);
+    }
+    else if ((baselineTemp == T_threshold))
+    {
+      lcd.setCursor(5, 0);
+      lcd.print("=");
+      delay(500);
+      digitalWrite(lampPin, LOW);
+    }
+    else
+    {
+      lcd.setCursor(5, 0);
+      lcd.print("<");
+      delay(500);
+      digitalWrite(lampPin, HIGH);
+    }
+
+    // Servo //
+    if (Hrs == 0 || Hrs == 1 || Hrs == 2 || Hrs == 3 ||
+        Hrs == 8 || Hrs == 9 || Hrs == 10 || Hrs == 11 ||
+        Hrs == 16 || Hrs == 17 || Hrs == 18 || Hrs == 19 || Hrs == 24)
+    {
+      if (onlyOnceRotate)
+      {
+        Serial.println(EEPROM.read(ServoPositionAddr));
+        if (EEPROM.read(ServoPositionAddr) >= 90)
+        {
+          servoPosition = EEPROM.read(ServoPositionAddr);
+          Serial.println("  ROTATING BACKWARD  ");
+          while (servoPosition > 0)
+          {
+            servo.write(servoPosition);
+            delay(21);
+            servoPosition -= 1;
+          }
+          servo.write(servoPosition);
+
+          EEPROM.write(ServoPositionAddr, servoPosition);
+          Serial.println(EEPROM.read(ServoPositionAddr));
+          delay(110);
+        }
+        onlyOnceRotate = false;
+      }
+    }
+    else if (Hrs == 4 || Hrs == 5 || Hrs == 6 || Hrs == 7 ||
+             Hrs == 12 || Hrs == 13 || Hrs == 14 || Hrs == 15 ||
+             Hrs == 20 || Hrs == 21 || Hrs == 22 || Hrs == 23)
+    {
+      if (onlyOnceRotate)
+      {
+        Serial.println(EEPROM.read(ServoPositionAddr));
+        if (EEPROM.read(ServoPositionAddr) < 90)
+        {
+          servoPosition = EEPROM.read(ServoPositionAddr);
+          Serial.println("  ROTATING FORWARD  ");
+          while (servoPosition < 90)
+          {
+            servo.write(servoPosition);
+            delay(21);
+            servoPosition += 1;
+          }
+          servo.write(servoPosition);
+
+          EEPROM.write(ServoPositionAddr, servoPosition);
+          Serial.println(EEPROM.read(ServoPositionAddr));
+          delay(110);
+        }
+        onlyOnceRotate = false;
+      }
+    }
+
+    timeTemp = timeTemp + (millis() - timeRunning);
+    while (timeTemp >= 1000)
+    {
+      timeTemp = timeTemp - 1000;
+      Sec = Sec + 1;
+      EEPROM.write(secondAddr, Sec);
+    }
+    while (Sec >= 60)
+    {
+      Sec = Sec - 60;
+      Min = Min + 1;
+      EEPROM.write(secondAddr, Sec);
+      EEPROM.write(minuteAddr, Min);
+    }
+    if (Min == 60)
+    {
+      Min = 0;
+      Hrs = Hrs + 1;
+
+      if (Hrs % 4 == 0)
+      {
+        onlyOnceRotate = true;
+      }
+      EEPROM.write(minuteAddr, Min);
+      EEPROM.write(hourAddr, Hrs);
+    }
+    if (Hrs == 24)
+    {
+      Hrs = 0;
+      Day = Day + 1;
+      EEPROM.write(hourAddr, Hrs);
+      EEPROM.write(dayAddr, Day);
+    }
   }
   else
   {
-    lcd.setCursor(5, 0);
-    lcd.print("<");
-    delay(500);
-
-    digitalWrite(lampPin, HIGH);
-  }
-  // Servo //
-  if (Hrs == 0 || Hrs == 1 || Hrs == 2 || Hrs == 3 ||
-      Hrs == 8 || Hrs == 9 || Hrs == 10 || Hrs == 11 ||
-      Hrs == 16 || Hrs == 17 || Hrs == 18 || Hrs == 19 || Hrs == 24)
-  {
-    if (onlyOnceRotate)
+    Serial.print(F("Failed to read _ reset: "));
+    Serial.println(AHT10Sensor.softReset());
+    if (AHT10Sensor.softReset() == 0)
     {
-      Serial.println(EEPROM.read(ServoPositionAddr));
-      if (EEPROM.read(ServoPositionAddr) >= 90)
-      {
-        servoPosition = EEPROM.read(ServoPositionAddr);
-        Serial.println("  ROTATING BACKWARD  ");
-        while (servoPosition > 0)
-        {
-          servo.write(servoPosition);
-          delay(21);
-          servoPosition -= 1;
-        }
-        servo.write(servoPosition);
-
-        EEPROM.write(ServoPositionAddr, servoPosition);
-        Serial.println(EEPROM.read(ServoPositionAddr));
-        delay(110);
-      }
-      onlyOnceRotate = false;
+      Serial.println("softReset is failed");
     }
-  }
-  else if (Hrs == 4 || Hrs == 5 || Hrs == 6 || Hrs == 7 ||
-           Hrs == 12 || Hrs == 13 || Hrs == 14 || Hrs == 15 ||
-           Hrs == 20 || Hrs == 21 || Hrs == 22 || Hrs == 23)
-  {
-    if (onlyOnceRotate)
+    else if (AHT10Sensor.softReset() == 1)
     {
-      Serial.println(EEPROM.read(ServoPositionAddr));
-      if (EEPROM.read(ServoPositionAddr) < 90)
-      {
-        servoPosition = EEPROM.read(ServoPositionAddr);
-        Serial.println("  ROTATING FORWARD  ");
-        while (servoPosition < 90)
-        {
-          servo.write(servoPosition);
-          delay(21);
-          servoPosition += 1;
-        }
-        servo.write(servoPosition);
-
-        EEPROM.write(ServoPositionAddr, servoPosition);
-        Serial.println(EEPROM.read(ServoPositionAddr));
-        delay(110);
-      }
-      onlyOnceRotate = false;
+      Serial.println("softReset is successful");
     }
+    Siren(true);
+    lcd.print("No Sensor data.");
+    lcd.setCursor(0, 1);
+    lcd.print("System Halted.");
+    digitalWrite(lampPin, LOW);
+    // digitalWrite(fanPin, LOW);
+    analogWrite(fanPin, 0);
   }
-  timeTemp = timeTemp + (millis() - timeRunning);
-  while (timeTemp >= 1000)
-  {
-    timeTemp = timeTemp - 1000;
-    Sec = Sec + 1;
-    EEPROM.write(secondAddr, Sec);
-  }
-  while (Sec >= 60)
-  {
-    Sec = Sec - 60;
-    Min = Min + 1;
-    EEPROM.write(secondAddr, Sec);
-    EEPROM.write(minuteAddr, Min);
-  }
-  if (Min == 60)
-  {
-    Min = 0;
-    Hrs = Hrs + 1;
-
-    if (Hrs % 4 == 0)
-    {
-      onlyOnceRotate = true;
-    }
-    EEPROM.write(minuteAddr, Min);
-    EEPROM.write(hourAddr, Hrs);
-  }
-  if (Hrs == 24)
-  {
-    Hrs = 0;
-    Day = Day + 1;
-    EEPROM.write(hourAddr, Hrs);
-    EEPROM.write(dayAddr, Day);
-  }
-
-  lcd.clear();
-
-  //}
-  //  else if (ack == 1)
-  //  {
-  //    lcd.clear();
-  //    lcd.print("No Sensor data.");
-  //    lcd.setCursor(0, 1);
-  //    lcd.print("System Halted.");
-  //    digitalWrite(lampPin, LOW);
-  //    //digitalWrite(fanPin, LOW);
-  //    analogWrite(fanPin,0);
-  //  }
 }
 
 void setTime()
@@ -459,7 +417,7 @@ void setTime()
     lcd.print("s:");
     lcd.print(Sec);
 
-    Serial.print("Set Time ");
+    Serial.println(F("Set Time "));
 
     while (Time_condition)
     {
@@ -667,7 +625,6 @@ void setTime()
       }
     }
   }
-  lcd.clear();
 }
 
 void setTempratureAndHumidity()
@@ -678,7 +635,8 @@ void setTempratureAndHumidity()
     lcd.print("Set Temperature:");
     lcd.setCursor(0, 1);
     lcd.print(T_threshold);
-    lcd.print(" *C");
+    lcd.write((byte)2);
+    lcd.print("C");
     Serial.print("Set Temperature ");
 
     while (T_condition)
@@ -691,7 +649,8 @@ void setTempratureAndHumidity()
         EEPROM.write(T_thresholdAddr, T_threshold);
         lcd.setCursor(0, 1);
         lcd.print(T_threshold);
-        lcd.print(" *C");
+        lcd.write((byte)2);
+        lcd.print("C");
         AutoSet = 0;
       }
       if (digitalRead(DOWN) == LOW) // Pressed
@@ -700,7 +659,8 @@ void setTempratureAndHumidity()
         EEPROM.write(T_thresholdAddr, T_threshold);
         lcd.setCursor(0, 1);
         lcd.print(T_threshold);
-        lcd.print(" *C");
+        lcd.write((byte)2);
+        lcd.print("C");
         AutoSet = 0;
       }
       if (digitalRead(ok) == LOW) // Pressed
@@ -797,5 +757,25 @@ void goToSetStatus()
     T_condition = true;
     H_condition = true;
     return;
+  }
+}
+
+void Siren(bool Enabled)
+{
+  if (Enabled)
+  {
+    for (int x = 0; x < 180; x++)
+    {
+      // convert degrees to radians then obtain sin value
+      sinVal = (sin(x * (3.1412 / 180)));
+      // generate a frequency from the sin value
+      toneVal = 2000 + (int(sinVal * 1000));
+      tone(7, toneVal);
+      delay(2);
+    }
+  }
+  else
+  {
+    noTone(7);
   }
 }
